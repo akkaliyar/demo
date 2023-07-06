22269b4685ba23696f3b52cc6f8415e18d0429f6
akkaliyar <117629654+akkaliyar@users.noreply.github.com>
2023-07-06 16:55:24 +0530

    change

--- dist/interfaces/Common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ISortOrder = void 0;
-var ISortOrder;
-(function (ISortOrder) {
-    ISortOrder["ASC"] = "ASC";
-    ISortOrder["DESC"] = "DESC";
-})(ISortOrder || (exports.ISortOrder = ISortOrder = {}));
--- dist/schemas/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const class_schema_1 = require("./class.schema");
-const student_schema_1 = require("./student.schema");
-const typeDefs = [
-    class_schema_1.classSchema,
-    student_schema_1.studentSchema
-];
-exports.default = typeDefs;

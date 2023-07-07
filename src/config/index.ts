688cac61f387d8d8b697b0b20b441eb38fcade00
akkaliyar <117629654+akkaliyar@users.noreply.github.com>
2023-07-07 11:15:27 +0530

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const CONFIG = Object({
     MONGO_HOST: process.env.MONGO_HOST || "mongodb://127.0.0.1:27017/",
     MONGO_DB: process.env.MONGO_DB || "class-mng",
-    CONNECTION_PORT: process.env.CONNECTION_PORT || 3000,
+    CONNECTION_PORT: process.env.CONNECTION_PORT || 4000,
     MONGO_URI() {
         //return this.MONGO_HOST + this.MONGO_DB
         return "mongodb://akhil:aohANHXUBx6kHWZkNeIfYkshW8QBj6t0eH2sNLJ7JgTrzuUUiRfrFuJRZExnclkipLQYWbH3JmulACDboBzL0w%3D%3D@akhil.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@akhil@";

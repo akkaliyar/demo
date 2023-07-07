03e7dccbe87061a1cd5c4092a0aeefe9eba0edd7
akkaliyar <117629654+akkaliyar@users.noreply.github.com>
2023-07-07 11:35:03 +0530

    change

--- dist/controllers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const class_controller_1 = require("./class.controller");
-const student_controller_1 = require("./student.controller");
-const resolvers = [
-    class_controller_1.classController,
-    student_controller_1.studentController
-];
-exports.default = resolvers;
